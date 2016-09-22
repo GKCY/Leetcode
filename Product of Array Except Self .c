33ae53f446740b3b4b41343dac6753a66ff3c08e
GKCY <GKCY@users.noreply.github.com>
2016-09-23 00:16:29 +0800

    Update Product of Array Except Self .c

--- Product of Array Except Self .c
+++ Product of Array Except Self .c
@@ -1,3 +1,10 @@
+//Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+//Solve it without division and in O(n).
+
+//For example, given [1,2,3,4], return [24,12,8,6].
+
+
 /**
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
