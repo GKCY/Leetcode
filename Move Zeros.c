82c2895a5af85ed221369efd158ad4d920a124d8
GKCY <GKCY@users.noreply.github.com>
2016-09-24 23:49:50 +0800

    Update Move Zeros.c

--- Move Zeros.c
+++ Move Zeros.c
@@ -1,3 +1,11 @@
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+1.You must do this in-place without making a copy of the array.
+2.Minimize the total number of operations.
+
 void moveZeroes(int* nums, int numsSize) {
     int pointer = 0;
     for(int i = 0;i<numsSize;i++)
