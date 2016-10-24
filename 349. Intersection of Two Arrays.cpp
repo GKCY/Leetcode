ab49901492608d6831959fcc342bbf86a2259c59
陈宇 <GKCY@users.noreply.github.com>
2016-10-24 22:16:57 +0800

    Create 349. Intersection of Two Arrays.cpp

---
+++ 349. Intersection of Two Arrays.cpp
@@ -0,0 +1,28 @@
+//Given two arrays, write a function to compute their intersection.
+//
+//Example:
+//Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+//
+//Note:
+//Each element in the result must be unique.
+//The result can be in any order.
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res;
+        unordered_map<int,int> map;
+        for(int i = 0;i < nums1.size();i++)
+            if(map[nums1[i]] == 0)
+                map[nums1[i]]++;
+        for(int i = 0;i < nums2.size();i++)
+        {
+            if(map[nums2[i]] == 1){
+                map[nums2[i]] = 0;
+                res.push_back(nums2[i]);
+            }
+        }
+        return res;
+    }
+};
