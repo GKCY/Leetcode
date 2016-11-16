1f6625945913508b21b4c729bbab02ea05c030b3
陈宇 <GKCY@users.noreply.github.com>
2016-11-16 16:23:56 +0800

    Create 496.玩具工厂(Toy Factory).py

---
+++ 496.玩具工厂(Toy Factory).py
@@ -0,0 +1,45 @@
+'''
+工厂模式是一种常见的设计模式。请实现一个玩具工厂 ToyFactory 用来产生不同的玩具类。可以假设只有猫和狗两种玩具。
+
+样例
+ToyFactory tf = ToyFactory();
+Toy toy = tf.getToy('Dog');
+toy.talk(); 
+>> Wow
+
+toy = tf.getToy('Cat');
+toy.talk();
+>> Meow
+'''
+
+"""
+Your object will be instantiated and called as such:
+ty = ToyFactory()
+toy = ty.getToy(type)
+toy.talk()
+"""
+class Toy:
+    def talk(self):
+        raise NotImplementedError('This method should have implemented.')
+
+class Dog(Toy):
+    # Write your code here
+    def talk(self):
+        print ("Wow")
+
+
+class Cat(Toy):
+    # Write your code here
+    def talk(self):
+        print("Meow")
+
+
+class ToyFactory:
+    # @param {string} shapeType a string
+    # @return {Toy} Get object of the type
+    def getToy(self, type):
+        # Write your code here
+        if type == 'Dog':
+            return Dog()
+        if type == 'Cat':
+            return Cat()
