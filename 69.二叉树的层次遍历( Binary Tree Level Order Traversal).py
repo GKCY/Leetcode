1322ade8ffe54294bbecb2535f4485d6d658fc06
陈宇 <GKCY@users.noreply.github.com>
2016-11-20 01:40:38 +0800

    Create 69.二叉树的层次遍历( Binary Tree Level Order Traversal).py

---
+++ 69.二叉树的层次遍历( Binary Tree Level Order Traversal).py
@@ -0,0 +1,30 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: Level order in a list of lists of integers
+    """
+    def levelOrder(self, root):
+        # write your code here
+        self.res = []
+        if root is None:
+            return self.res
+        q = [root]
+        while q:
+            new_q = []
+            self.res.append([n.val for n in q])
+            for node in q:
+                if node.left:
+                    new_q.append(node.left)
+                if node.right:
+                    new_q.append(node.right)
+            q = new_q
+        return self.res
