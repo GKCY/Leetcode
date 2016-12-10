ad0c5dbfbe049a9f7b21528602032e638f290801
陈宇 <GKCY@users.noreply.github.com>
2016-12-10 16:27:27 +0800

    Create 371.在O(1)时间复杂度删除链表节点(delete node in the middle of singly linked list).py

---
+++ 371.在O(1)时间复杂度删除链表节点(delete node in the middle of singly linked list).py
@@ -0,0 +1,19 @@
+#给定一个单链表中的一个等待被删除的节点(非表头或表尾)。请在在O(1)时间复杂度删除该链表节点。
+
+"""
+Definition of ListNode
+class ListNode(object):
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+class Solution:
+    # @param node: the node in the list should be deleted
+    # @return: nothing
+    def deleteNode(self, node):
+        # write your code here
+        next = node.next
+        node.val = next.val
+        node.next = next.next
+        return
